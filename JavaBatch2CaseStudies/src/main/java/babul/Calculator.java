60796e99be52096a59ae82ab842a2a054c765c3a
Babul Kumar Dwivedy <62852798+bkdwivedy@users.noreply.github.com>
2022-07-16 11:59:34 +0530

    Calculator.java

---
+++ JavaBatch2CaseStudies/src/main/java/babul/Calculator.java
@@ -0,0 +1,17 @@
+package trainingTaskCalculator;
+
+interface Calculator {
+
+	int addition(int input1, int input2);
+
+	int substraction(int input1, int input2);
+
+	int multiplication(int input1, int input2);
+
+	int division(int input1, int input2);
+
+	int modules(int input1, int input2);
+
+	int percentage(int input1, int input2);
+
+}
